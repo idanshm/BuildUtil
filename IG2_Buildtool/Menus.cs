e4f1513b76949025e7608be37de6e64c604503cb
Idan Shamami <idans@codevalue.net>
2018-10-21 14:02:40 +0300

    First commit

---
+++ IG2_Buildtool/Menus.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IG2_Buildtool
+{
+    public enum MainMenuOptions
+    {
+        Build = 1,
+        Clean = 2,
+        Exit = 0
+    }
+
+    public enum BuildMenuClientOptions
+    {
+        All = 1,
+        Italy = 2,
+        Germany = 3,
+        Back = 9,
+        Exit = 0
+    }
+
+    public enum BuildMenuActionOptions
+    {
+        Build = 1,
+        Rebuild = 2,
+        Back = 9,
+        Exit = 0
+    }
+
+    public static class Menus
+    {
+        public static MainMenuOptions DisplyMainMenu()
+        {
+            Console.WriteLine("####Welcome to IG2 Build Tool####\n");
+            Console.WriteLine("Select an option from the list:");
+            Console.WriteLine("1. Build");
+            Console.WriteLine("2. Clean");
+            Console.WriteLine("0. Exit");
+            Console.Write("Action: ");
+            int.TryParse(Console.ReadLine(), out int selection);
+            return Enum.Parse<MainMenuOptions>(selection.ToString());
+        }
+
+        public static BuildMenuClientOptions DisplayBuildClientMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Select client from the list:");
+            Console.WriteLine("1. All");
+            Console.WriteLine("2. Italy");
+            Console.WriteLine("2. Germany");
+            Console.WriteLine("9. Back");
+            Console.WriteLine("0. Exit");
+            Console.Write("Action: ");
+            int.TryParse(Console.ReadLine(), out int selection);
+            return Enum.Parse<BuildMenuClientOptions>(selection.ToString());
+        }
+
+        public static BuildMenuActionOptions DisplayBuildActionMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Select build action from the list:");
+            Console.WriteLine("1. Build");
+            Console.WriteLine("2. Rebuild");
+            Console.WriteLine("9. Back");
+            Console.WriteLine("0. Exit");
+            Console.Write("Action: ");
+            int.TryParse(Console.ReadLine(), out int selection);
+            return Enum.Parse<BuildMenuActionOptions>(selection.ToString());
+        }
+    }
+}
---
+++ IG2_Buildtool/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace IG2_Buildtool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            switch (Menus.DisplyMainMenu())
+            {
+                case (MainMenuOptions.Build):
+                    {
+                        BuildMenuClientOptions retValue = Menus.DisplayBuildClientMenu();
+                        NextStepClientBuildMenu(retValue);
+                        break;
+                    }
+                case (MainMenuOptions.Clean):
+                    {
+                        break;
+                    }
+                case (MainMenuOptions.Exit):
+                    {
+                        Environment.Exit(0);
+                        break;
+                    }
+            }
+            
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static void NextStepClientBuildMenu(BuildMenuClientOptions retValue)
+        {
+            switch (retValue)
+            {
+                case BuildMenuClientOptions.All:
+                case BuildMenuClientOptions.Italy:
+                case BuildMenuClientOptions.Germany:
+                    BuildMenuActionOptions retValue2 = Menus.DisplayBuildActionMenu();
+                    NextStepActionBuildMenu(retValue2);
+                    break;
+                case BuildMenuClientOptions.Back:
+                    Main(null);
+                    break;
+                case BuildMenuClientOptions.Exit:
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+
+        private static void NextStepActionBuildMenu(BuildMenuActionOptions retValue2)
+        {
+            switch (retValue2)
+            {
+                case BuildMenuActionOptions.Build:
+                case BuildMenuActionOptions.Rebuild:
+                    break;
+                case BuildMenuActionOptions.Back:
+                    BuildMenuClientOptions retValue = Menus.DisplayBuildClientMenu();
+                    NextStepClientBuildMenu(retValue);
+
+
+                    break;
+                case BuildMenuActionOptions.Exit:
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+    }
+}
